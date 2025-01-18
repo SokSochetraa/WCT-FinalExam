f3195d64fafbb10a2f3985afd1fa3b9a3940dbf1
សុខ​ សុចិត្រា - Sok Sochetra <sok.sochetra.2822@rupp.edu.kh>
2025-01-18 15:18:54 +0700

    Add files via upload

---
+++ Strange/stragne.js
@@ -0,0 +1,15 @@
+function createCounter() {
+  let count = 0;
+
+  return function () {
+    console.log("Student Name: Sochetra Sok");
+    count++;
+    return count;
+  };
+}
+
+const counter = createCounter();
+
+console.log(counter());
+console.log(counter());
+console.log(counter());
